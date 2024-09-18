a956f06c3730b0d59f816bb457016a48ea609dad
PaulArq <paul.arquier@edu.ece.fr>
2024-09-18 17:52:16 +0200

    correctif

--- src/components/Experience/Timeline.jsx
+++ src/components/Experience/Timeline.jsx
@@ -1,40 +1,40 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import styles from "./Timeline.module.css";
 import { getImageUrl } from "../../utils";
-import {LanguageContext} from "../../LanguageContext.jsx";
+import { LanguageContext } from "../../LanguageContext.jsx";
 import timelineData from "../../data/timelineData.json";
-import {translations} from "../../data/translations.js";
+import { translations } from "../../data/translations.js";
 
 export const Timeline = () => {
-    const {language} = useContext(LanguageContext);
+    const { language } = useContext(LanguageContext);
     const data = timelineData[language];
     const t = translations[language].timeline;
 
     return (
         <>
             <h2 className={styles.title}>{t.title}</h2>
-        <div className={styles.timelineContainer}>
-            <div className={styles.timeline}>
-                {data.map((item, index) => (
-                    <div key={index} className={styles.timelineItem}>
-                        <div
-                            className={`${styles.timelineContent} ${index % 2 === 0 ? styles.left : styles.right}`}
-                        >
-                            <img
-                                src={getImageUrl(item.logoSrc)}
-                                alt={item.title}
-                                className={styles.timelineLogo}
-                            />
-                            <div className={styles.timelineDetails}>
-                                <p className={styles.timelineDate}>{item.date}</p>
-                                <h3 className={styles.timelineTitle}>{item.title}</h3>
-                                <p className={styles.timelineDescription}>{item.description}</p>
+            <div className={styles.timelineContainer}>
+                <div className={styles.timeline}>
+                    {data.map((item, index) => (
+                        <div key={index} className={styles.timelineItem}>
+                            <div className={styles.timelineContent}>
+                                <img
+                                    src={getImageUrl(item.logoSrc)}
+                                    alt={item.title}
+                                    className={styles.timelineLogo}
+                                />
+                                <div className={styles.timelineDetails}>
+                                    <p className={styles.timelineDate}>{item.date}</p>
+                                    <h3 className={styles.timelineTitle}>{item.title}</h3>
+                                    <p className={styles.timelineDescription}>
+                                        {item.description}
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        </div>
         </>
     );
 };
